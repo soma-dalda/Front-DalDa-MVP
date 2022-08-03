ffcd14eb2c4f6acd0c0bab41f773a1e48029ec04
jaewoong2 <jwisgenius@naver.com>
2022-08-04 00:30:29 +0900

    :lipstick: Card 컴포넌트 구현

--- src/pages/Serach/Card/index.tsx
+++ src/pages/Serach/Card/index.tsx
@@ -1,33 +1,36 @@
-import { Image } from '@jaewoong2/dui'
 import React from 'react'
+import { Image } from '@jaewoong2/dui'
 
-function Card() {
+function Card({
+  src,
+  description,
+  href,
+  button,
+}: {
+  src: string
+  description: string
+  href: string
+  button: string
+}) {
   return (
     <div className="max-w-[100%] bg-white rounded-lg border">
-      <a href="/">
-        <Image
-          isLoading={false}
-          className="p-8 rounded-t-lg"
-          src="https://flowbite.com/docs/images/products/apple-watch.png"
-          alt="product image"
-        />
+      <a href={href}>
+        <Image isLoading={false} className="p-8 rounded-t-lg" src={src} alt="product image" />
       </a>
       <div className="px-5 pb-5">
-        <a href="/">
-          <h5 className="text-sm font-semibold tracking-tight text-gray-900">
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-          </h5>
+        <a href={href}>
+          <h5 className="text-sm font-semibold tracking-tight text-gray-900">{description}</h5>
         </a>
 
-        <div className="flex justify-end mt-4 flex-col">
+        <button aria-label="바로가기" type="button" className="flex justify-end mt-4 flex-col">
           <a
-            href="/"
+            href={href}
             className="bg-[#FBF7F2] hover:outline outline-gray-300 
             focus:ring-4 focus:outline font-semibold rounded-lg text-sm px-5 py-2.5 text-center"
           >
-            바로가기
+            {button}
           </a>
-        </div>
+        </button>
       </div>
     </div>
   )
