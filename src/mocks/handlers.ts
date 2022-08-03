71c3c5e5df95338bf4af2ecb7aafbbbdb3e5bd8e
jaewoong2 <jwisgenius@naver.com>
2022-08-04 00:29:17 +0900

    :clown_face: mock 파일구조변경 및 search 관련 mock data 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,43 +1,9 @@
 import { rest } from 'msw'
+import { loginGoogle, loginKakao, loginNaver, search } from './requests'
 
 export const handlers = [
-  rest.get('/login/naver', async (_, res, ctx) => {
-    return res(
-      ctx.status(400),
-      ctx.json({
-        error: {
-          body: 'invalid Login',
-        },
-      })
-    )
-  }),
-
-  rest.get('/login/kakao', async (_, res, ctx) => {
-    return res(
-      ctx.delay(4000),
-      ctx.json({
-        user: {
-          profile: {
-            nickname: 'kakao',
-          },
-          email: 'dalda@kakao.com',
-          phone_number: '010-1234-5678',
-        },
-      })
-    )
-  }),
-
-  rest.get('/login/google', async (_, res, ctx) => {
-    return res(
-      ctx.json({
-        user: {
-          profile: {
-            nickname: 'google',
-          },
-          email: 'dalda@google.com',
-          phone_number: '010-1234-5678',
-        },
-      })
-    )
-  }),
+  rest.get('/login/naver', loginNaver),
+  rest.get('/login/kakao', loginKakao),
+  rest.get('/login/google', loginGoogle),
+  rest.get('/search/:keyword', search),
 ]
---
+++ src/mocks/requests.ts
@@ -0,0 +1,68 @@
+import { DefaultBodyType, PathParams, ResponseComposition, RestContext, RestRequest } from 'msw'
+
+export const loginNaver = async (
+  _: RestRequest<never, PathParams<string>>,
+  res: ResponseComposition<DefaultBodyType>,
+  ctx: RestContext
+) => {
+  return res(
+    ctx.status(400),
+    ctx.json({
+      error: {
+        body: 'invalid Login',
+      },
+    })
+  )
+}
+
+export const loginKakao = async (
+  _: RestRequest<never, PathParams<string>>,
+  res: ResponseComposition<DefaultBodyType>,
+  ctx: RestContext
+) => {
+  return res(
+    ctx.delay(4000),
+    ctx.json({
+      user: {
+        profile: {
+          nickname: 'kakao',
+        },
+        email: 'dalda@kakao.com',
+        phone_number: '010-1234-5678',
+      },
+    })
+  )
+}
+
+export const loginGoogle = async (
+  _: RestRequest<never, PathParams<string>>,
+  res: ResponseComposition<DefaultBodyType>,
+  ctx: RestContext
+) => {
+  return res(
+    ctx.json({
+      user: {
+        profile: {
+          nickname: 'google',
+        },
+        email: 'dalda@google.com',
+        phone_number: '010-1234-5678',
+      },
+    })
+  )
+}
+
+export const search = async (
+  req: RestRequest<never, PathParams<string>>,
+  res: ResponseComposition<DefaultBodyType>,
+  ctx: RestContext
+) => {
+  return res(
+    ctx.delay(2000),
+    ctx.json({
+      keyword: req.params.keyword,
+      src: 'https://flowbite.com/docs/images/products/apple-watch.png',
+      description: 'Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport',
+    })
+  )
+}
